38c2413121e881ff79a1f75f302b6cf8de511184
pbcn2 <huaxuan_2002@outlook.com>
2023-04-27 22:31:04 +0800

    update RTT

--- No.1_TCP&UDP/UDP/Release_user.py
+++ No.1_TCP&UDP/UDP/Release_user.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import threading 
 import time
 
-HOST = '172.28.53.135'  # 服务器连接地址
+HOST = '172.28.63.133'  # 服务器连接地址
 PORT = 8080  # 服务器启用端口
 BUFSIZ = 1024  # 缓冲区大小
 ADDR = (HOST, PORT)
@@ -19,31 +19,23 @@ ADDA = 0  # 目的地信息
 def recv_timeout():
     pass  #为了配合函数调用变量需要设置的无用函数
 
-def delta(time_start, time_end):
-    # 将时间戳bytes字符串转换为字符串，并使用切片操作获取时间戳部分
-    time_start = time_start.decode('utf-8')[1:14]
-    time_end = time_end.decode('utf-8')[1:14]
-
-    # 将时间戳字符串转换为datetime对象
-    dt1 = datetime.fromtimestamp(int(time_start) / 1000)
-    dt2 = datetime.fromtimestamp(int(time_end) / 1000)
-    print(dt1, dt2)
-
-    # 计算两个datetime对象之间的时间差
-    delta = dt2 - dt1
-    return delta
+def timestamp_diff_ms(timestamp1, timestamp2):  # 两个时间戳时间差的函数，用于计算RTT
+    dt1 = datetime.fromtimestamp(timestamp1 / 1000 + (timestamp1 % 1000) / 1000000)
+    dt2 = datetime.fromtimestamp(timestamp2 / 1000 + (timestamp2 % 1000) / 1000000)
+    diff_seconds = (dt2 - dt1).total_seconds()
+    diff_ms = diff_seconds * 1000
+    return diff_ms
 
 print("正在ping", HOST, "端口号", PORT)
 
 for i in range(10):
-    data = '1'
-    send_time = int(time.time() * 1000 + time.time_ns() % 1000000 // 1000)
-    # 构造新的时间戳字符串
-    send_time_str = bytes('[%d] %s' % (send_time, data), encoding='utf-8')
-    # data = bytes(send_time_str, encoding="UTF-8")  # 发送时间戳信息
+    data = '1'  # 模拟传输的报文
+    data = bytes(data, encoding='utf-8')  # 将字符中解码为bytes类型
+    # 创建一个发送时间的时间戳，小数点后6位级别的
+    send_timestamp = int(time.time() * 1000 + time.time_ns() // 1000000 % 1000 + time.time_ns() % 1000000 / 1000000)
 
     ADDR = (HOST, PORT)  # 在每次循环中更新ADDR的值
-    udpCliendSocket.sendto(send_time_str, ADDR)  # 调用发送接口
+    udpCliendSocket.sendto(data, ADDR)  # 调用发送接口
     udpCliendSocket.settimeout(1)  # 设置超时时间为1秒
     timer = threading.Timer(1.1, recv_timeout)  # 创建定时器线程，超时时间为1秒
     timer.start()  # 启动定时器线程
@@ -54,9 +46,9 @@ for i in range(10):
             time_start = data
         time_end = data
         count += 1  # 计数器加一
-        print(send_time_str,time_end)
-        print("连接成功...", delta(send_time_str, time_end))
-        continue
+        # 创建接受时间时间戳
+        recv_timestamp = int(time.time() * 1000 + time.time_ns() // 1000000 % 1000 + time.time_ns() % 1000000 / 1000000)
+        print("连接成功...", "RTT =", timestamp_diff_ms(send_timestamp, recv_timestamp), "ms")
     except timeout:
         print("请求超时。")  # 超时输出
         continue  # 超时后使用continue跳过本轮循环
@@ -65,7 +57,7 @@ for i in range(10):
         timer.cancel()  # 取消定时器线程
 
 
-# 将时间戳bytes字符串转换为字符串，并使用切片操作获取时间戳部分
+# 将服务器时间戳bytes字符串转换为字符串，并使用切片操作获取时间戳部分
 if time_start:
     # 将时间戳bytes字符串转换为字符串，并使用切片操作获取时间戳部分
     time_start = time_start.decode('utf-8')[1:14]
